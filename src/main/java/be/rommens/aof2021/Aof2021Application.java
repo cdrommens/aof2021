537b96d4c789dbc548bef3afc973709c067de5c0
Cederik Rommens <cederik.rommens@kbc.be>
2022-11-15 15:50:04 +0100

    initial setup

---
+++ src/main/java/be/rommens/aof2021/Aof2021Application.java
@@ -0,0 +1,13 @@
+package be.rommens.aof2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aof2021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aof2021Application.class, args);
+    }
+
+}
